4c1c9d373bc8ad184af06756be37e3f4798eeb7c
Kleber777 <klebermirandalima@hotmail.com>
2021-01-08 03:21:29 -0300

    Create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Hello from './Hello'
+import Counter from './Counter'
+import { useEffect } from 'react';
+
+function App() {
+  return (
+    // <Hello message="Kleber Miranda Lima"/>
+    <h1>Hello</h1>
+
+  )
+}
+
+export default App;
